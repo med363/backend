0bc7ee0c02115a2a12a26d033f8ea4bb964fc499
med3633 <sallemomar@sfax.r-iset.tn>
2025-10-18 17:45:26 +0100

    chge upload path fro work proof

--- src/Artisana/artoffre.entity.ts
+++ src/Artisana/artoffre.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Artisan } from '../auth/entities/artisan.entity';
 
 @Entity('artoffres')
@@ -16,19 +17,11 @@ export class ArtOffre {
   prix: number;
 
   @Column({ type: 'varchar', nullable: true })
-  imageProofOfWork: string;
+  imageProofOfWork?: string;
 
   @Column({ type: 'text', nullable: true })
-  images: string;
+  images?: string;
 
-  // ManyToOne relationship with Artisan
-  @ManyToOne(() => Artisan, (artisan) => artisan.artoffres, { 
-    onDelete: 'CASCADE'
-  })
-  @JoinColumn({ name: 'artisanId' }) // This creates the foreign key
+  @ManyToOne(() => Artisan, (artisan) => artisan.artoffres, { onDelete: 'CASCADE' })
   artisan: Artisan;
-
-  // Add this column - it's required for the foreign key
-  @Column()
-  artisanId: number;
 }
